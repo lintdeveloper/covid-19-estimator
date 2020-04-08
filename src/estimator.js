f66a0c1a684e50f0d5306fc0d00e9c73af581160
Musa Musa <musamusa739@gmail.com>
2020-04-08 19:21:01 +0100

    added spaces before and after braces during restructing

--- src/estimator.js
+++ src/estimator.js
@@ -2,7 +2,7 @@ const covid19ImpactEstimator = (data) => {
   const inputData = data;
   const REPORTED_CONST = 10;
   const SEVERE_INFECTED_CONST = 50;
-  const {reportedCases} = inputData;
+  const { reportedCases } = inputData;
   const currentlyInfected = reportedCases * REPORTED_CONST;
   const severeCurrentlyInfected = reportedCases * SEVERE_INFECTED_CONST;
   const INFECTATION_CONST = 1024;
