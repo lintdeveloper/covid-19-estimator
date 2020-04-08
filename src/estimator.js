cca1ef8d396945eeb268aa431937a6a798e519bf
Musa Musa <musamusa739@gmail.com>
2020-04-08 19:18:25 +0100

    removed unexpected dangling

--- src/estimator.js
+++ src/estimator.js
@@ -3,44 +3,21 @@ const covid19ImpactEstimator = (data) => {
   const REPORTED_CONST = 10;
   const SEVERE_INFECTED_CONST = 50;
   const {reportedCases} = inputData;
-  const _currentlyInfected = reportedCases * REPORTED_CONST;
-  const _severeCurrentlyInfected = reportedCases * SEVERE_INFECTED_CONST;
+  const currentlyInfected = reportedCases * REPORTED_CONST;
+  const severeCurrentlyInfected = reportedCases * SEVERE_INFECTED_CONST;
   const INFECTATION_CONST = 1024;
 
-  return {
+  return { 
     data: inputData,
     impact: {
       currentlyInfected: _currentlyInfected,
-      infectionsByRequestedTime: _currentlyInfected * INFECTATION_CONST
+      infectionsByRequestedTime: currentlyInfected * INFECTATION_CONST
     },
     severeImpact: {
-      currentlyInfected: _severeCurrentlyInfected,
-      infectionsByRequestedTime: _severeCurrentlyInfected * INFECTATION_CONST
+      currentlyInfected: severeCurrentlyInfected,
+      infectionsByRequestedTime: severeCurrentlyInfected * INFECTATION_CONST
     }
   };
 };
 
 export default covid19ImpactEstimator;
-
-
-// Input Data
-// {
-//     region: {
-//     name: "Africa",
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//     },
-//     periodType: "days",
-//     timeToElapse: 58,
-//     reportedCases: 674,
-//     population: 66622705,
-//     totalHospitalBeds: 1380614
-// }
-
-// output data
-// {
-//     data: {}, // the input data you got
-//     impact: {}, // your best case estimation
-//     severeImpact: {} // your severe case estimation
-//  }
