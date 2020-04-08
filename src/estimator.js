1386c259bf0913844023003649662eec3cd89f7a
Musa Musa <musamusa739@gmail.com>
2020-04-08 18:12:21 +0100

    start estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,33 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const inputData = data;
+    return {
+        data: inputData,
+        impact: {},
+        severeImpact: {}
+    }
+};
 
 export default covid19ImpactEstimator;
+
+
+// Input Data
+// {
+//     region: {
+//     name: "Africa",
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: "days",
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// }
+
+// output data
+// {
+//     data: {}, // the input data you got
+//     impact: {}, // your best case estimation
+//     severeImpact: {} // your severe case estimation
+//  }
