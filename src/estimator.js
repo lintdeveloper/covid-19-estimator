3e37ae99789f6e0fd7bd5bf5e7ee885d75500a20
Musa Musa <musamusa739@gmail.com>
2020-04-10 08:57:30 +0100

    changed from const to let

--- src/estimator.js
+++ src/estimator.js
@@ -2,9 +2,9 @@ const covid19ImpactEstimator = (data) => {
   const inputData = data;
   const REPORTED_CONST = 10;
   const SEVERE_INFECTED_CONST = 50;
-  const { reportedCases } = inputData;
-  const currentlyInfected = reportedCases * REPORTED_CONST;
-  const severeCurrentlyInfected = reportedCases * SEVERE_INFECTED_CONST;
+  let { reportedCases } = inputData;
+  let currentlyInfected = reportedCases * REPORTED_CONST;
+  let severeCurrentlyInfected = reportedCases * SEVERE_INFECTED_CONST;
   const INFECTATION_CONST = 1024;
   return {
     data: inputData,
